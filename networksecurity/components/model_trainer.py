b57bc6f3647422f75f103ab1bc2bed71bd8c8e99
zizokosaa <zeyadsalih333@gmail.com>
2025-04-22 22:31:02 +0200

    Model Training and Evaluation Completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,14 +22,14 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
-"""from urllib.parse import urlparse
+from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+dagshub.init(repo_owner='ZeyadSaleh-ejust', repo_name='networksecurity', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"""
+os.environ['MLFLOW_TRACKING_URI'] = "https://dagshub.com/ZeyadSaleh-ejust/networksecurity.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="ZeyadSaleh-ejust"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="0a814d4b710305469e1d136c3e175e425874d6af"
 
 
 
@@ -43,30 +43,20 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def track_mlflow(self,best_model,classificationmetric):
-        #mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
-        #tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    def track_mlflow(self, best_model, train_metrics, test_metrics):
+        mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
-            f1_score=classificationmetric.f1_score
-            precision_score=classificationmetric.precision_score
-            recall_score=classificationmetric.recall_score
-
-            
-
-            mlflow.log_metric("f1_score",f1_score)
-            mlflow.log_metric("precision",precision_score)
-            mlflow.log_metric("recall_score",recall_score)
-            mlflow.sklearn.log_model(best_model,"model")
-            # Model registry does not work with file store
-            """if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
-            else:
-                mlflow.sklearn.log_model(best_model, "model")"""
+            # Log training metrics
+            mlflow.log_metric("train_f1_score", train_metrics.f1_score)
+            mlflow.log_metric("train_precision", train_metrics.precision_score)
+            mlflow.log_metric("train_recall", train_metrics.recall_score)
+            # Log test metrics
+            mlflow.log_metric("test_f1_score", test_metrics.f1_score)
+            mlflow.log_metric("test_precision", test_metrics.precision_score)
+            mlflow.log_metric("test_recall", test_metrics.recall_score)
+            # Log model with correct registered name
+            mlflow.sklearn.log_model(best_model, "model", registered_model_name=type(best_model).__name__)
 
 
         
@@ -121,13 +111,14 @@ class ModelTrainer:
         # for training data
         y_train_pred=best_model.predict(X_train)
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
-        self.track_mlflow(best_model,classification_train_metric)
+        #self.track_mlflow(best_model,classification_train_metric)
 
         # for testing data
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
-        self.track_mlflow(best_model,classification_test_metric)
+        #self.track_mlflow(best_model,classification_test_metric)
 
+        self.track_mlflow(best_model,classification_train_metric,classification_test_metric)
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
         # making directory for the best model    
@@ -136,9 +127,9 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         # saving the best model as pickle file
-        save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
         #model pusher
-        #save_object("final_model/model.pkl",best_model)
+        save_object("final_model/model.pkl",best_model)
         
 
         ## Model Trainer Artifact
